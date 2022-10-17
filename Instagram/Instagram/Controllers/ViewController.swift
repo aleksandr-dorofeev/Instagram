1143ce4132890096ba1e6f35a80c02fb4182bd30
dorofeevs <aleksandr.dorofeev@akvelon.com>
2022-10-17 19:12:45 +0400

    Initial commit

---
+++ Instagram/Instagram/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Aleksandr Dorofeev on 17.10.2022.
+//
+
+import UIKit
+
+/// Feed screen.
+class FeedViewController: UIViewController {
+
+  // MARK: - Life cycle.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
